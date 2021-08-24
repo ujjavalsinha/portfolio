93b0d4c28147f8b1545edd6c6eade80ca72b5c62
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2021-08-24 16:29:55 +0530

    Add files via upload

---
+++ src/components/Main/Main.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Main.css'
+import { HashLink as Link } from 'react-router-hash-link';
+import {ReactComponent as InstagramIcon} from '../../assets/instagram.svg';
+import { ReactComponent as GithubIcon } from '../../assets/github.svg';
+import { ReactComponent as LinkedinIcon } from '../../assets/linkedin.svg'
+const Main = props => {
+  return (
+    <div id='home' className="Main">
+      
+      <div className='icons-list'>
+        <a rel="noopener noreferrer" target="_blank"><InstagramIcon className='icons'/></a>
+        <a rel="noopener noreferrer" target="_blank" href='https://www.linkedin.com/in/ujjaval-sinha-6794453a/'><LinkedinIcon className='icons'/></a>
+        <a rel="noopener noreferrer" target="_blank"><GithubIcon className='icons'/></a>
+        <a rel="noopener noreferrer" target="_blank"><InstagramIcon className='icons'/></a>
+        <div className='line'></div>
+      </div>
+      <div className='main-sub'>
+        <p className='hi-message'>Hi, my name is</p>
+        <h1 className='name'>
+          <span className='letter'>U</span>
+          <span className='letter'>J</span>
+          <span className='letter'>J</span>
+          <span className='letter'>A</span>
+          <span className='letter'>V</span>
+          <span className='letter'>A</span>
+          <span className='letter'>L</span>
+          <span className='letter'>&nbsp;</span>
+          <span className='letter'>S</span>
+          <span className='letter'>I</span>
+          <span className='letter'>N</span>
+          <span className='letter'>H</span>
+          <span className='letter'>A</span>
+          </h1>
+        <p className='message'>I build things for web</p>
+        <p className='paragraph'>I'm a software engineer specializing in building exceptional digital experiences. I am currently working as a web developer at Infosys</p>
+        <Link to='#about' smooth><button className='get-in-touch'>Get In Touch</button></Link>
+      </div>
+    </div>
+  )
+}
+
+export default Main
