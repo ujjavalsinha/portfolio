a3e9d03df0e8fa9cff0c9cd485f56f999e28ab7e
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2021-08-24 19:30:14 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ const Projects = props => {
             <div className='project-details right-details'>
               <p className='featured-project'>Featured Project</p>
               <p className='project-name'>Digital Doctor</p>
-              <p className='project-description' data-aos="zoom-in-left">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.by the readable content of a page when  'Content here, content here', making it look like readable English. </p>
+              <p className='project-description' data-aos="zoom-in-left">A online medical consultation web application for COVID 19 quick check. Based on the symptoms and medical conditions, it predicts the amount of risk to COVID </p>
               <div className='tech-stack'>
                 <p>React</p>
                 <p>Django</p>
@@ -48,7 +48,7 @@ const Projects = props => {
             <div className='project-details left-details'>
               <p className='featured-project left '>Featured Project</p>
               <p className='project-name left'>My Blog</p>
-              <p className='project-description left left-description' data-aos="zoom-in-right">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.by the readable content of a page when  'Content here, content here', making it look like readable English. </p>
+              <p className='project-description left left-description' data-aos="zoom-in-right">It is a blog application where user can signup, login, create, edit, read and delete posts. User can also comment on other blogger's posts. It is built using Reactjs and Flask </p>
               <div className='tech-stack left-tech-stack'>
                 <p>React</p>
                 <p>Flask</p>
@@ -69,7 +69,7 @@ const Projects = props => {
             <div className='project-details right-details'>
               <p className='featured-project'>Featured Project</p>
               <p className='project-name'>Todo App</p>
-              <p className='project-description' data-aos="zoom-in-left">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.by the readable content of a page when  'Content here, content here', making it look like readable English. </p>
+              <p className='project-description' data-aos="zoom-in-left">A basic todo app where user can add, edit, read and delete tasks to be done and track it. User can choose or add a category to the task which the user is adding </p>
               <div className='tech-stack'>
                 <p>React</p>
                 <p>Django</p>
@@ -90,7 +90,7 @@ const Projects = props => {
             <div className='project-details left-details'>
               <p className='featured-project left '>Featured Project</p>
               <p className='project-name left'>Corona Count Tracker</p>
-              <p className='project-description left left-description' data-aos="zoom-in-right">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.by the readable content of a page when  'Content here, content here', making it look like readable English. </p>
+              <p className='project-description left left-description' data-aos="zoom-in-right">It is a corona tracker application which gives the count of active cases and deceased of all the countries in the world. Using covid 19 public API for retrieving data. It also shows a graphical representation of the cases</p>
               <div className='tech-stack left-tech-stack'>
                 <p>React</p>
                 <p>CSS</p>
