aafee0e17ed12278bbeb03a86630c7255e50c7bb
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2021-08-24 16:36:17 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css'
+const Footer = props => {
+    return (
+        <div className='Footer' >
+            <div className='footer-main' >
+            <div className='footer-left-line' ></div>
+            <p className='footer-text' >
+                Design and built by Ujjaval Sinha
+            </p>
+            <div className='footer-right-line' ></div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
